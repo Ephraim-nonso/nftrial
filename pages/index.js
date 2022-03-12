10f11142774e5d30a24cf8e67ebd532507be252e
Fafemi Adeola <adeola5678@gmail.com>
2022-03-12 11:32:50 +0100

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/navbar";
 import { Container } from "../components/container";
-
-export default function Home() {
-  return (
-  <div>
-    <Navbar />
-    <Container>
-      <h1 className="text-3xl font-bold underline">HomePage</h1>
-    </Container>
 import LandingContent from "../components/landing-content";
-import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
