7d93e4335c6db25309e0542544cd93c35453fb22
Adegbite Ademola kelvin <Adegbiteademola1999@gmail.com>
2022-03-12 16:39:48 +0100

    ft: implement aboutus page

---
+++ components/wallet.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from "next/image";
+import { BsArrowRight, BsArrowLeft } from "react-icons/bs"
+import Link from "next/link";
+
+
+function Wallet() {
+  return (
+    <React.Fragment>
+          <div className="flex items-center mb-10 justify-between">
+        <Link href="/" passHref={true}>
+          <button><BsArrowLeft className="text-white" font-size={35} /></button>
+          </Link>
+          <h4 className="sm:text-2xl font-grotesk text-left text-white md:font-normal md:text-5xl">Connect your wallet to log in</h4>
+        </div>
+        <div className="sm:grid  p-4  md:grid grid-cols-2 gap-8 ">
+          <div className="mb-4 bg-slate-50 rounded-lg flex items-center justify-between p-4">
+            <div className="flex  items-center ">
+              <Image src="/assets/walletConnect-icon.svg" width={100} height={100} />
+              <h4 className="ml-4 font-grotesk font-normal text-2xl">walletconnects</h4>
+            </div>
+            <button><BsArrowRight font-size={35} /></button>
+          </div>
+          <div className="mb-4 bg-slate-50 rounded-lg flex items-center justify-between p-4">
+            <div className="flex  items-center">
+              <Image src="/assets/Vector.svg" width={100} height={100} />
+              <h4 className="ml-4 font-grotesk font-bold text-4xl">status</h4>
+            </div>
+            <button><BsArrowRight font-size={35} /></button>
+          </div>
+          <div className="mb-4 bg-slate-50 rounded-lg flex items-center justify-between p-4">
+            <div className="flex  items-center">
+              <Image src="/assets/TWT.svg" width={100} height={100} />
+              <h4 className="ml-4 font-grotesk font-normal text-2xl">trust Wallet</h4>
+            </div>
+            <button><BsArrowRight font-size={35} /></button>
+          </div>
+          <div className="mb-4 bg-slate-50 rounded-lg flex items-center justify-between p-4">
+            <div className="flex  items-center">
+              <Image src="/assets/metamask.svg" width={100} height={100} />
+              <h4 className="ml-4 font-grotesk font-normal text-2xl">metamask</h4>
+            </div>
+            <button><BsArrowRight font-size={35} /></button>
+          </div>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Wallet
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AboutUs from "../components/about";
 
 function About() {
-  return <div>About</div>;
+ return(
+  <div className="w-full flex justify-center items-center p-10 xl:p-24">
+    <AboutUs/>
+    </div>
+ )
 }
 
 export default About;
