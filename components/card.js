b3d8137072d522780357331fe1913baf06380a9e
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-03-12 15:47:51 +0100

    Responsiveness of GS/Success page

--- components/card.js
+++ components/card.js
@@ -5,154 +5,154 @@ const Card = () => {
     <div className="my-5">
       <div className="flex my-8">
         <div
-          className="h-24 w-10 p-7 cursor-pointer item-center justify-center mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg item-center justify-center mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto content-center justify-center">
+          <h4 className="font-medium font-grotesk text-align mx-auto content-center justify-center">
             1A
           </h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">5E</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">5E</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">4C</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">4C</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">2B</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">2B</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">2F</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">2F</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">6D</h4>
-        </div>
-      </div>
-      <div className="flex my-7">
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">6C</h4>
-        </div>
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">3D</h4>
-        </div>
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">2A</h4>
-        </div>
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">5F</h4>
-        </div>
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">4E</h4>
-        </div>
-        <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
-          onClick={() => {
-            alert();
-          }}
-        >
-          <h4 className="font-medium text-align mx-auto">1B</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">6D</h4>
         </div>
       </div>
       <div className="flex my-7">
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">1B</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">6C</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">2D</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">3D</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">5E</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">2A</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">4A</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">5F</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">3C</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">4E</h4>
         </div>
         <div
-          className="h-24 w-10 p-7 cursor-pointer  mx-1 bg-slate-400 shadow-lg "
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
           onClick={() => {
             alert();
           }}
         >
-          <h4 className="font-medium text-align mx-auto">6F</h4>
+          <h4 className="font-medium font-grotesk text-align mx-auto">1B</h4>
+        </div>
+      </div>
+      <div className="flex my-7">
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">1B</h4>
+        </div>
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">2D</h4>
+        </div>
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">5E</h4>
+        </div>
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">4A</h4>
+        </div>
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">3C</h4>
+        </div>
+        <div
+          className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg "
+          onClick={() => {
+            alert();
+          }}
+        >
+          <h4 className="font-medium font-grotesk text-align mx-auto">6F</h4>
         </div>
       </div>
     </div>
--- pages/gamescreen.js
+++ pages/gamescreen.js
@@ -1,30 +1,41 @@
 import React from "react";
 import Card from "../components/card";
 import { Container } from "../components/container";
+import { BsInfoLg } from "react-icons/bs";
 
 function GameScreen() {
   return (
     <Container>
-      <div className="flex justify-between py-12">
+      <div className="sm:flex sm:items-center justify-between py-12">
         <div>
           <div className="font-cont">
-            <h1 className="text-white text-4xl font-bold">Game arena</h1>
+            <h1 className="text-white text-4xl font-bold font-grotesk">
+              Game arena
+            </h1>
           </div>
 
           <Card />
         </div>
 
-        <div className="flex flex-col justify-end items-end">
+        <div className="sm:items-center lg:items-end flex flex-col justify-end">
           <div
-            className="info text-3xl rounded-3xl bg-white p-6 h-12 w-12 text-center cursor-pointer"
+            className=" info text-3xl rounded-3xl bg-white p-3 h-11 w-11 text-center place-items-center cursor-pointer"
             onClick={() => {
               alert();
             }}
           >
-            <h4>?</h4>
+            <div>
+              <BsInfoLg className="text-black" font-size={20} />
+            </div>
           </div>
-          <div className="h-96 w-80 p-10 bg-slate-400 mt-5 content-center">
-            <h3>Hello</h3>
+          <div className="h-96 w-80 flex justify-between items-center p-10 bg-slate-400 mt-5 content-center">
+            <h3 className="text-xl font-bold font-grotesk">A1</h3>
+            <h3 className="text-xl font-bold font-grotesk">1B</h3>
+            <h3 className="text-xl font-bold font-grotesk">2D</h3>
+            <h3 className="text-xl font-bold font-grotesk">3F</h3>
+            <h3 className="text-xl font-bold font-grotesk">4E</h3>
+            <h3 className="text-xl font-bold font-grotesk">5C</h3>
+            <h3 className="text-xl font-bold font-grotesk">6A</h3>
           </div>
         </div>
       </div>
