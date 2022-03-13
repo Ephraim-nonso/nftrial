ed3b874811261dcc50f6d0916fe84e8600c39a9a
Fafemi Adeola <adeola5678@gmail.com>
2022-03-13 14:28:22 +0100

    ft(cards)

--- components/card.js
+++ components/card.js
@@ -1,29 +1,102 @@
 import React, { useState } from "react";
 import "./utils/GameArena.json";
+import gsap from "gsap";
 
 const Card = () => {
   const [change, setChange] = useState(false);
+  const ref = React.useRef([]);
+  ref.current = [];
+  const addToRefs = (el) => {
+    if (!ref.current.includes(el)) {
+      ref.current.push(el);
+    }
+  };
+  React.useEffect(() => {
+    ref.current.forEach(
+      (el) => {
+        const container = el.querySelector("div");
+
+        el.addEventListener("click", (e) => {
+          gsap.to(container, { backgroundColor: "red" });
+        });
+      },
+      [ref]
+    );
+  });
 
   const flip = () => {
     setChange(!change);
   };
+  const cardData = [
+    {
+      value: "1A",
+    },
+    {
+      value: "2A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+    {
+      value: "1A",
+    },
+  ];
 
   return (
     <div className="my-5">
       <div className="flex my-8">
-        <div
-          // style={{ backgroundColor: change ? "red" : "#94A3B8" }}
-          className="h-24 p-7 cursor-pointer max-w-sm md:max-w-lg item-center bg-slate-400 justify-center mx-1  shadow-lg hover:border-solid hover:border-2 hover:border-yellow-500"
-          onClick={(e) => {
-            setChange(!change);
-            console.log(e);
-          }}
-        >
-          <h4 className="font-medium font-grotesk text-align mx-auto content-center justify-center">
-            1A
-          </h4>
-        </div>
-        <div
+        {cardData.length > 1 &&
+          cardData.map(({ value }) => {
+            return (
+              <div key={value} ref={addToRefs}>
+                <div className="h-24 p-7 cursor-pointer max-w-sm md:max-w-lg item-center bg-slate-400 justify-center mx-1  shadow-lg hover:border-solid hover:border-2 hover:border-yellow-500">
+                  <h4 className="font-medium font-grotesk text-align mx-auto content-center justify-center">
+                    {value}
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
+        {/* <div
           // style={{ backgroundColor: change ? "red" : "#94A3B8" }}
           className="h-24  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg hover:border-solid hover:border-2 hover:border-yellow-500"
           onClick={() => {
@@ -141,7 +214,7 @@ const Card = () => {
           }}
         >
           <h4 className="font-medium font-grotesk text-align mx-auto">5E</h4>
-        </div>
+        </div> */}
         <div
           className="h-24 w-18  p-7 cursor-pointer max-w-sm md:max-w-lg  mx-1 bg-slate-400 shadow-lg hover:border-solid hover:border-2 hover:border-yellow-500"
           onClick={() => {
